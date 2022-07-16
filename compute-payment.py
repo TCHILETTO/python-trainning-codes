ea2642aa797d950b46395faa50342080404f4510
Tatiana Chiletto <tatiana.chiletto@gmail.com>
2022-07-16 14:52:40 -0400

    Adding compute payment code

---
+++ compute-payment.py
@@ -0,0 +1,14 @@
+# Code used to calculate the payment, related to working hours,
+# using 40 hour as weekly limit. Above that, the rate is multiplied
+# by 1,5.
+
+def computepay(hours, rate):
+    hourThreshold = 40
+    if hours <= hourThreshold:
+        return hours * rate
+    else:
+        return (hourThreshold * rate) + (hours - hourThreshold) * 1.5 * rate
+
+hrs = input("Enter Hours:")
+rate = input ("Enter Rate:")
+print("Pay", computepay(float(hrs), float(rate)))
